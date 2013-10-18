d7d0a7269975744085ed4752a645a97afb72700f
minyoon0205 <minyoon0205@gmail.com>
2013-10-18 04:09:26 -0700

    buggy date parser based on URL

--- cal_event_scraper.py
+++ cal_event_scraper.py
@@ -18,12 +18,14 @@ csvwriter = csv.writer(open('./'+fpath+'.csv', 'w')) # open csv
 c = conn.cursor()
 
 # Set up for the csv and sqlite3 files
-c.execute('''CREATE TABLE today (\n\ttitle text,\n\t link text,\n\t desc text)''')
-csvwriter.writerow(['title', 'link', 'desc'])
+c.execute('''CREATE TABLE today (\n title text,\n date date,\n link text,\n desc text)''')
+csvwriter.writerow(['title', 'date', 'link', 'desc'])
 
 for e in d.entries:
-    c.execute("INSERT INTO today VALUES (?,?,?)", (e.title,e.link,e.description,))
-    csvwriter.writerow([e.title.encode('ascii', 'ignore'), e.link.encode('ascii', 'ignore'), e.description.encode('ascii', 'ignore')])
+    link_split = e.link.split('&date=')
+    date = link_split[1]
+    c.execute("INSERT INTO today VALUES (?,?,?,?)", (e.title,date, e.link,e.description,))
+    csvwriter.writerow([e.title.encode('ascii', 'ignore'), date, e.link.encode('ascii', 'ignore'), e.description.encode('ascii', 'ignore')])
 
 conn.commit()
 conn.close()
